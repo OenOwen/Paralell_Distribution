113c5051f20c08d060a21066b6c8aa5292a334de
William Abrahamsson <william_abrahamsson@outlook.com>
2025-02-11 16:51:32 +0100

    Added file for a2 called optimized version

---
+++ Assignment 1/optimized_mx_mlt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+// allows us to use cout instead of std::cout every time.
+using namespace std;
+
+
+int main() {
+    cout << "Hello opttimized version \n";
+    return 1;
+}
